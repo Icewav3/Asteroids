0e1a04ec0a1f53ae3daff78ad88292f00c218ff9
Cole Dorman <coledorman12@gmail.com>
2025-03-05 11:38:49 -0600

    almost working

--- classes/GameObject.js
+++ classes/GameObject.js
@@ -1,3 +1,5 @@
+// noinspection JSSuspiciousNameCombination
+
 class GameObject {
     /**
      * Creates a new instance of the object with the specified parameters.
@@ -8,48 +10,80 @@ class GameObject {
      * @param {list} vertices - The vertices of the polygon
      * @param {boolean} isActive - Indicates whether the object is active.
      */
-  constructor(position, velocity = createVector(0, 0), rotation, vertices, color, isActive){
-    this.position = position;
-    this.velocity = velocity;
-    this._rotation = rotation;
-    this.vertices = vertices;
-    this.normals = normals; //todo
-    this.color = color;
-    this._isActive = isActive;
+    constructor(position, velocity = createVector(0, 0), rotation, vertices, color, isActive = true) {
+        this.position = position;
+        this.velocity = velocity;
+        this._rotation = rotation;
+        this.vertices = vertices;
+        this.color = color;
+        this._isActive = isActive;
+
+        this.normals = this.calculateNormals();
     }
 
-    update(){
-      if(this._isActive){
-          this.position += this.velocity;
-      }
-    }
-
-    draw(){
-      if(this._isActive){
-        push();
-        fill(this.color);
-        beginShape();
-        for (let v of this.vertices) {
-          vertex(v.x + this.position.x, v.y + this.position.y);
+    update() {
+        if (this._isActive) {
+            this.position = p5.Vector.add(this.position, this.velocity);
+            //print(this.velocity)
         }
-        endShape(CLOSE);
-        pop();
-      }
+        else{
+          print("This object isn't active dumbass")
+        }
     }
 
-    checkPolygonCollision(gameObject){
-      let min = Infinity, max = -Infinity;
-
-      for (let v of gameObject.vertices) {
-        let projection = v.add(this.position).dot(this.normal); // Project the vertex onto the axis
-        if (projection < min) min = projection;
-        if (projection > max) max = projection;
-      }
-
-      return [min, max]; // Return the min/max projection range
+    draw() {
+        if (this._isActive) {
+            push();
+            fill(this.color);
+            beginShape();
+            for (let v of this.vertices) {
+                vertex(v.x + this.position.x, v.y + this.position.y);
+            }
+            endShape(CLOSE);
+            pop();
+        }
+        else{
+          print("This object isn't active dumbass")
+        }
     }
 
-    destroy(){
+    calculateNormals() {
+        let normals = [];
+        for (let i = 0; i < this.vertices.length; i++) {
+            // Get current vertex and next vertex
+            let current = this.vertices[i];
+            let next = this.vertices[(i + 1) % this.vertices.length];
 
+            // Calculate edge vector
+            let edge = p5.Vector.sub(next, current);
+
+            // Calculate normal (perpendicular vector)
+            let normal = createVector(-edge.y, edge.x).normalize();
+            normals.push(normal);
+        }
+        return normals;
+    }
+
+    checkPolygonCollision(self, collidingGameObject) {
+        let isColliding = true;
+        let min1 = Infinity, max1 = -Infinity;
+        let min2 = Infinity, max2 = -Infinity;
+        let combinedNormals = self.normals.concat(collidingGameObject.normals);
+        for (let i = 0; i < self.normals.length; i++) {
+            for (let v of self.vertices) {
+                let projection =  p5.Vector.add(v, self.position).dot(combinedNormals[i]); // Project the vertex onto
+                // the axis
+                if (projection < min1) min1 = projection;
+                if (projection > max1) max1 = projection;
+            }
+            for (let v of collidingGameObject.vertices) {
+                let projection =  p5.Vector.add(v, collidingGameObject.position).dot(combinedNormals[i]); // Project the vertex onto
+                // the axis
+                if (projection < min2) min2 = projection;
+                if (projection > max2) max2 = projection;
+            }
+            isColliding &&= ((min1 < max2 && min1 > min2) || (min2 < max1 && min2 > min1))//&&= === (var = bool && bool)
+        }
+        return isColliding;
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,11 +1,69 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
+let obj1, obj2;
+let obj2Rotation = 0; // Variable to store rotation for obj2
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    // Create two GameObject instances with some sample data
+    obj1 = new GameObject(
+        createVector(200, 200), // Position
+        createVector(0, 0),     // Velocity
+        0,                      // Rotation
+        [
+            createVector(-20, -20),
+            createVector(20, -20),
+            createVector(20, 20),
+            createVector(-20, 20)
+        ],                      // Vertices
+        color(255, 0, 0),       // Color
+    );
+
+    obj2 = new GameObject(
+        createVector(mouseX, mouseY), // Position (updated to start at mouse position)
+        createVector(0, 0),          // Static, so no velocity
+        0,                           // Rotation
+        [
+            createVector(-30, -30),
+            createVector(30, -30),
+            createVector(30, 30),
+            createVector(-30, 30)
+        ],                           // Vertices
+        color(0, 0, 255),            // Color
+    );
 }
 
 function draw() {
-  background(220);
+    background(220);
+
+    // Update obj1's position
+    obj1.update();
+    obj1.draw();
+
+    // Update obj2 to follow the mouse position and apply rotation
+    obj2.position.set(mouseX, mouseY); // Follow mouse position
+    obj2.rotation = obj2Rotation;     // Apply rotation
+    obj2.draw();
+
+    // Display collision debug information
+    let isColliding = obj1.checkPolygonCollision(obj1, obj2);
+
+    // Display collision status on the canvas
+    noStroke();
+    fill(0);
+    textSize(20);
+    textAlign(CENTER);
+    text(
+        isColliding ? "Collision Detected!" : "No Collision",
+        width / 2,
+        height - 30
+    );
+}
+
+// Listen to mouseWheel event to rotate obj2
+function mouseWheel(event) {
+    obj2Rotation += event.delta > 0 ? 0.1 : -0.1; // Adjust rotation by mouse wheel
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
 }
